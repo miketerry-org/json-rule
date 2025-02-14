ae46363c16939d446c97cf4b653b2c47c3564d0c
Mike Terry <support@miketerry.org>
2025-02-14 12:29:44 -0600

    corrected bugs introduced by sloppy find/replace

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 // index.js:
 
+"use strict";
+
 // load all necessary modules
-const PropertyRule = require("./lib/propertyRuleRule");
-const ObjectRuleRule = require("./lib/objectRuleRule");
-const ArrayRuleRule = require("./lib/arrayRuleRule");
-const BooleanRuleRule = require("./lib/booleanRuleRule");
-const DateRuleRule = require("./lib/dateRuleRule");
-const EmailRuleRule = require("./lib/emailRuleRule");
-const EnumerationRuleRule = require("./lib/enumerationRuleRule");
-const FloatRuleRule = require("./lib/floatRuleRule");
-const IntegerRuleRule = require("./lib/integerRuleRule");
-const StringRuleRule = require("./lib/stringRuleRule");
-const TimeRuleRule = require("./lib/timeRuleRule");
-const TimestampRuleRule = require("./lib/timestampRuleRule");
+const PropertyRule = require("./lib/propertyRule");
+const ObjectRule = require("./lib/objectRule");
+const ArrayRule = require("./lib/arrayRule");
+const BooleanRule = require("./lib/booleanRule");
+const DateRule = require("./lib/dateRule");
+const EmailRule = require("./lib/emailRule");
+const EnumerationRule = require("./lib/enumerationRule");
+const FloatRule = require("./lib/floatRule");
+const IntegerRule = require("./lib/integerRule");
+const StringRule = require("./lib/stringRule");
+const TimeRule = require("./lib/timeRule");
+const TimestampRule = require("./lib/timestampRule");
 
 // export all property rule  classes
 module.exports = {
-  ArrayRuleRule,
-  BooleanRuleRule,
-  DateRuleRule,
-  EmailRuleRule,
-  EnumerationRuleRule,
-  FloatRuleRule,
-  IntegerRuleRule,
-  ObjectRuleRule,
-  PropertyRuleRule,
-  StringRuleRule,
-  TimeRuleRule,
-  TimestampRuleRule,
+  ArrayRule,
+  BooleanRule,
+  DateRule,
+  EmailRule,
+  EnumerationRule,
+  FloatRule,
+  IntegerRule,
+  ObjectRule,
+  PropertyRule,
+  StringRule,
+  TimeRule,
+  TimestampRule,
 };
